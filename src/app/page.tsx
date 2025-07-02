dd384aa81f3d0e7cff9e2e0f8a10c32b1ed8f401
Cloudyday56 <Rchen78900@gmail.com>
2025-07-02 10:48:38 -0400

    Project setup #2
    Authentication with Clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+import { SignedIn, SignedOut, SignInButton, SignOutButton } from '@clerk/nextjs'
 import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>HomePage
-    
+    <div>
+      HomePage
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+
+      <SignedIn>
+        <SignOutButton />
+      </SignedIn>
     </div>
   )
 }
